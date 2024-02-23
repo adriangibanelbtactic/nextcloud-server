3da664645a74909ed800c683805b03c9a6a90892
Julius Härtl <jus@bitgrid.net>
2024-02-23 11:00:44 +0100

    fixup! fix: Allow nonce in csp header also if no other reasons are given

--- lib/public/AppFramework/Http/EmptyContentSecurityPolicy.php
+++ lib/public/AppFramework/Http/EmptyContentSecurityPolicy.php
@@ -37,8 +37,8 @@ namespace OCP\AppFramework\Http;
  * @since 9.0.0
  */
 class EmptyContentSecurityPolicy {
-	/** @var string JS nonce to be used */
-	protected $jsNonce = null;
+	/** @var ?string JS nonce to be used */
+	protected ?string $jsNonce = null;
 	/** @var bool Whether strict-dynamic should be used */
 	protected $strictDynamicAllowed = null;
 	/** @var bool Whether strict-dynamic should be used on script-src-elem */
